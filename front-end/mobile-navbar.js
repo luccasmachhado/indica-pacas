ca9cb36d2c21d186e4a3c16ff6927a2748ffd4af
Victor Valentim <victorvalentim0803@gmail.com>
2024-12-19 21:17:32 -0300

    Menu Feito!
    -Arrumei a programação do menu
    -Funcionando normal

--- front-end/mobile-navbar.js
+++ front-end/mobile-navbar.js
@@ -1,13 +1,44 @@
-class MobileNavbar{
-    constructor(mobileMenu, navlist) {
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
-        this.navlist = document.querySelector(navlist);
-        this.navLinks = document.querySelectorAll(this.navLinks);
-        this.activeClass= "active";
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
     }
-    
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click",()=>console.log
-        ("Hey"));
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                      index / 7 + 0.3
+                  }s`);
+        });
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
     }
 }
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li"
+);
+mobileNavbar.init();
